2e93c907422bf5f44b9276f04f438c09a6a1f56d
Yahor Zhylinski <zhilinskyegor@gmail.com>
2017-04-30 20:31:35 +0300

    [DEV] Add Sign as brand image

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,7 +17,9 @@ export default class Navbar extends React.Component {
               <span className="icon-bar"></span>
               <span className="icon-bar"></span>
             </button>
-            <a className="navbar-brand" href="#">Brand</a>
+            <Link className="navbar-brand" to="/">
+              <img src="img/sign.png" />
+            </Link>
           </div>
 
           <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
